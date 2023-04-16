2105f4792010c9aac7767ddf2f845c11e6f5bb66
LipeSeed <108769222+LipeSeed@users.noreply.github.com>
2023-04-16 18:25:18 -0300

    feat/delete

--- Clinicas/urls.py
+++ Clinicas/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path("delete/<int:cod_cli>/", delete_clinica, name="delete"),
     path("delete/<int:cod_med>/", delete_medico, name="delete"),
     path("delete/<int:cod_espec>/", delete_especialidade, name="delete"),
-    path("delete/<int:unique_together>/", delete_especialidade, name="delete"),
+    path("delete/<int:unique_together>/", delete_meta, name="delete"),
 ]
